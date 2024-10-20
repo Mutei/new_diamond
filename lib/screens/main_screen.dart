59a7a4fd14815545a2606d8bf000bb6f1ca7d0f3
Mutei Abuzahra <mutea200048@gmail.com>
2024-10-20 12:48:29 +0300

    37 commit bottom navigation bar 3

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,11 +1,10 @@
+import 'package:diamond_host_admin/widgets/reused_appbar.dart';
 import 'package:flutter/material.dart';
-import 'package:icons_plus/icons_plus.dart';
 import '../backend/customer_rate_services.dart';
 import '../backend/estate_services.dart';
 import '../localization/language_constants.dart';
-import '../widgets/estate_card_widget.dart';
-import '../widgets/reused_appbar.dart';
 import '../widgets/custom_drawer.dart';
+import '../widgets/estate_card_widget.dart';
 import 'profile_estate_screen.dart';
 import 'all_posts_screen.dart';
 import 'notification_screen.dart';
@@ -19,103 +18,26 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  EstateServices estateServices = EstateServices();
-  CustomerRateServices customerRateServices = CustomerRateServices();
-  List<Map<String, dynamic>> estates = [];
-  final List<String> categories = ['Hotel', 'Restaurant', 'Coffee'];
-
-  // Index for managing the bottom navigation
   int _selectedIndex = 0;
 
-  @override
-  void initState() {
-    super.initState();
-    _fetchEstates();
-  }
+  final List<Widget> _screens = [
+    const MainScreenContent(),
+    const UpgradeAccountScreen(),
+    const NotificationScreen(),
+    const AllPostsScreen(),
+  ];
 
-  Future<void> _fetchEstates() async {
-    try {
-      final data = await estateServices.fetchEstates();
-      List<Map<String, dynamic>> parsedEstates = _parseEstates(data);
-
-      // Fetch ratings for each estate
-      for (var estate in parsedEstates) {
-        final ratings =
-            await customerRateServices.fetchEstateRatingWithUsers(estate['id']);
-
-        // Calculate average rating
-        double totalRating = 0;
-        if (ratings.isNotEmpty) {
-          totalRating = ratings
-                  .map((e) => e['rating'] as double)
-                  .reduce((a, b) => a + b) /
-              ratings.length;
-        }
-
-        setState(() {
-          estate['rating'] =
-              totalRating; // Update estate with the average rating
-          estate['ratingsList'] = ratings; // Save the list of ratings
-        });
-      }
-
-      setState(() {
-        estates = parsedEstates;
-      });
-    } catch (e) {
-      print("Error fetching estates: $e");
-    }
-  }
-
-  List<Map<String, dynamic>> _parseEstates(Map<String, dynamic> data) {
-    List<Map<String, dynamic>> estateList = [];
-    data.forEach((key, value) {
-      value.forEach((estateID, estateData) {
-        estateList.add({
-          'id': estateID,
-          'nameEn': estateData['NameEn'] ?? 'Unknown',
-          'nameAr': estateData['NameAr'] ?? 'غير معروف', // Arabic fallback
-          'rating': 0.0, // Will be updated with actual rating later
-          'fee': estateData['Fee'] ?? 'Free',
-          'time': estateData['Time'] ?? '20 min',
-          'TypeofRestaurant': estateData['TypeofRestaurant'] ?? 'Unknown Type',
-          'Sessions': estateData['Sessions'] ?? 'Unknown Session Type',
-          'MenuLink': estateData['MenuLink'] ?? 'No Menu',
-          'Entry': estateData['Entry'] ?? 'Empty',
-          'Lstmusic': estateData['Lstmusic'] ?? 'No music',
-          'Type': estateData['Type'] ?? 'Unknown',
-        });
-      });
-    });
-    return estateList;
-  }
-
-  // Handle bottom navigation bar taps
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
   }
 
-  // List of screens for each bottom navigation tab
-  final List<Widget> _screens = [
-    const MainScreenContent(), // Main screen content
-    const UpgradeAccountScreen(),
-    const NotificationScreen(),
-    const AllPostsScreen(),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: ReusedAppBar(
-        title: getTranslated(context, "Main Screen"),
-      ),
       drawer: const CustomDrawer(),
-      body: IndexedStack(
-        index: _selectedIndex,
-        children: _screens,
-      ),
+      body: _screens[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
@@ -123,16 +45,16 @@ class _MainScreenState extends State<MainScreen> {
         unselectedItemColor: Colors.grey,
         items: const [
           BottomNavigationBarItem(
-            icon: Icon(Bootstrap.house_down),
+            icon: Icon(Icons.home),
             label: 'Main Screen',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.update),
+            icon: Icon(Icons.upgrade),
             label: 'Upgrade Account',
           ),
           BottomNavigationBarItem(
-            icon: Icon(TeenyIcons.book),
-            label: 'Booking Status',
+            icon: Icon(Icons.notifications),
+            label: 'Notifications',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.post_add),
@@ -144,7 +66,7 @@ class _MainScreenState extends State<MainScreen> {
   }
 }
 
-// MainScreenContent separated to keep the logic intact
+// Main Screen Content with its own AppBar
 class MainScreenContent extends StatefulWidget {
   const MainScreenContent({super.key});
 
@@ -174,7 +96,6 @@ class _MainScreenContentState extends State<MainScreenContent> {
         final ratings =
             await customerRateServices.fetchEstateRatingWithUsers(estate['id']);
 
-        // Calculate average rating
         double totalRating = 0;
         if (ratings.isNotEmpty) {
           totalRating = ratings
@@ -184,9 +105,8 @@ class _MainScreenContentState extends State<MainScreenContent> {
         }
 
         setState(() {
-          estate['rating'] =
-              totalRating; // Update estate with the average rating
-          estate['ratingsList'] = ratings; // Save the list of ratings
+          estate['rating'] = totalRating;
+          estate['ratingsList'] = ratings;
         });
       }
 
@@ -205,8 +125,8 @@ class _MainScreenContentState extends State<MainScreenContent> {
         estateList.add({
           'id': estateID,
           'nameEn': estateData['NameEn'] ?? 'Unknown',
-          'nameAr': estateData['NameAr'] ?? 'غير معروف', // Arabic fallback
-          'rating': 0.0, // Will be updated with actual rating later
+          'nameAr': estateData['NameAr'] ?? 'غير معروف',
+          'rating': 0.0,
           'fee': estateData['Fee'] ?? 'Free',
           'time': estateData['Time'] ?? '20 min',
           'TypeofRestaurant': estateData['TypeofRestaurant'] ?? 'Unknown Type',
@@ -223,115 +143,120 @@ class _MainScreenContentState extends State<MainScreenContent> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Categories Section
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              getTranslated(context, "All Categories"),
-              style: const TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+    return Scaffold(
+      appBar: ReusedAppBar(
+        title: getTranslated(context, "Main Screen"),
+      ),
+      drawer: const CustomDrawer(),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                getTranslated(context, "All Categories"),
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 120,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: categories.length,
-              itemBuilder: (context, index) {
-                IconData iconData;
-                switch (categories[index]) {
-                  case 'Hotel':
-                    iconData = Icons.hotel;
-                    break;
-                  case 'Restaurant':
-                    iconData = Icons.restaurant;
-                    break;
-                  case 'Coffee':
-                    iconData = Icons.local_cafe;
-                    break;
-                  default:
-                    iconData = Icons.category;
-                }
+            SizedBox(
+              height: 120,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: categories.length,
+                itemBuilder: (context, index) {
+                  IconData iconData;
+                  switch (categories[index]) {
+                    case 'Hotel':
+                      iconData = Icons.hotel;
+                      break;
+                    case 'Restaurant':
+                      iconData = Icons.restaurant;
+                      break;
+                    case 'Coffee':
+                      iconData = Icons.local_cafe;
+                      break;
+                    default:
+                      iconData = Icons.category;
+                  }
 
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: Column(
-                    children: [
-                      Container(
-                        width: 115,
-                        height: 80,
-                        decoration: BoxDecoration(
-                          color: Colors.grey[200],
-                          shape: BoxShape.circle,
-                        ),
-                        child: Icon(
-                          iconData,
-                          size: 40,
-                          color: Colors.deepPurple,
-                        ),
-                      ),
-                      const SizedBox(height: 8),
-                      Text(
-                        categories[index],
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
-          ),
-          estates.isEmpty
-              ? const Center(child: CircularProgressIndicator())
-              : ListView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  itemCount: estates.length,
-                  itemBuilder: (context, index) {
-                    final estate = estates[index];
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => ProfileEstateScreen(
-                              nameEn: estate['nameEn'],
-                              nameAr: estate['nameAr'],
-                              estateId: estate['id'],
-                              location: "Rose Garden",
-                              rating: estate['rating'],
-                              fee: estate['fee'],
-                              deliveryTime: estate['time'],
-                              price: 32.0,
-                              typeOfRestaurant: estate['TypeofRestaurant'],
-                              sessions: estate['Sessions'],
-                              menuLink: estate['MenuLink'],
-                              entry: estate['Entry'],
-                              music: estate['Lstmusic'],
-                              type: estate['Type'],
-                            ),
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Column(
+                      children: [
+                        Container(
+                          width: 115,
+                          height: 80,
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            shape: BoxShape.circle,
                           ),
-                        );
-                      },
-                      child: EstateCard(
-                        nameEn: estate['nameEn'],
-                        estateId: estate['id'],
-                        nameAr: estate['nameAr'],
-                        rating: estate['rating'], // Pass dynamic rating
-                      ),
-                    );
-                  },
-                ),
-        ],
+                          child: Icon(
+                            iconData,
+                            size: 40,
+                            color: Colors.deepPurple,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          categories[index],
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            estates.isEmpty
+                ? const Center(child: CircularProgressIndicator())
+                : ListView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: estates.length,
+                    itemBuilder: (context, index) {
+                      final estate = estates[index];
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ProfileEstateScreen(
+                                nameEn: estate['nameEn'],
+                                nameAr: estate['nameAr'],
+                                estateId: estate['id'],
+                                location: "Rose Garden",
+                                rating: estate['rating'],
+                                fee: estate['fee'],
+                                deliveryTime: estate['time'],
+                                price: 32.0,
+                                typeOfRestaurant: estate['TypeofRestaurant'],
+                                sessions: estate['Sessions'],
+                                menuLink: estate['MenuLink'],
+                                entry: estate['Entry'],
+                                music: estate['Lstmusic'],
+                                type: estate['Type'],
+                              ),
+                            ),
+                          );
+                        },
+                        child: EstateCard(
+                          nameEn: estate['nameEn'],
+                          estateId: estate['id'],
+                          nameAr: estate['nameAr'],
+                          rating: estate['rating'],
+                        ),
+                      );
+                    },
+                  ),
+          ],
+        ),
       ),
     );
   }
