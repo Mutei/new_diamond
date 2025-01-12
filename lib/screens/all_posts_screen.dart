74a1c227e5251fe64401fd87b0c7ad5a2736151d
Mutei Abuzahra <mutea200048@gmail.com>
2025-01-12 15:30:28 +0300

    final touches part 31

--- lib/screens/all_posts_screen.dart
+++ lib/screens/all_posts_screen.dart
@@ -268,7 +268,9 @@ class _AllPostsScreenState extends State<AllPostsScreen> {
                 padding:
                     const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
                 decoration: BoxDecoration(
-                  color: Colors.grey[200],
+                  color: Theme.of(context).brightness == Brightness.dark
+                      ? kDarkModeColor
+                      : Colors.grey[200],
                   borderRadius: BorderRadius.circular(20),
                 ),
                 child: Text(
